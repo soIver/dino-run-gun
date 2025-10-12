4840abc28043305408a4fb6912c64557318eb791
maris-volk <vpoiskeznanij@gmail.com>
2025-10-12 23:59:37 +0300

    Создание классов для динозавра и препятствий (#1)
    Closes #1

---
+++ entities/init.py
@@ -0,0 +1,10 @@
+# Инициализация пакета entities
+from dinosaur import Dinosaur
+from fireball import Fireball
+from obstacles import Obstacle
+
+__all__ = [
+    'Dinosaur',
+    'Fireball',
+    'Obstacle',
+]
